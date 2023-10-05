a69aa6170993f17663c92751ed293104817e5b4b
sohaib37104 <sohaib@softlinks.ae>
2023-10-06 00:25:27 +0500

    meal plan detail added

--- lib/src/profileSection/screens/myAccountScreen.dart
+++ lib/src/profileSection/screens/myAccountScreen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_share/flutter_share.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:healthify_care_provider/src/mealPlansSection/screens/meal_plan_listing_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../../../common/helperFunctions/navigatorHelper.dart';
@@ -357,7 +358,7 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
                         suffixicon: Res.arrowforward,
                         ontap: () {
                           toNext(
-                              context: context, widget: CreateMealPlanScreen());
+                              context: context, widget: MealPlansListingScreen());
                         },
                       ),
 
